7f86032d114a31771ed95b82fa51c3dcef271781
jovanialexanderbesa0920-creator <jovanialexanderbesa0920@gmail.com>
2026-03-24 15:51:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ if st.session_state.texto_pdfs:
 
         try:
             api_key = st.secrets["GEMINI_KEY"]
-            url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={api_key}"
+            url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={api_key}"
             payload = {
                 "contents": [{
                     "parts": [{
